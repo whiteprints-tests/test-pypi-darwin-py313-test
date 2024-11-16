b034fb1911aff24fa28ee459fb141998b9fa8f49
Romain Brault <mail@romainbrault.com>
2024-11-16 04:16:44 +0000

    feat: ✨ add command line interface

--- src/test_pypi_darwin_py313_test/__init__.py
+++ src/test_pypi_darwin_py313_test/__init__.py
@@ -4,10 +4,25 @@
 
 """Top-level module."""
 
+import importlib
 from typing import Final
 
+from test_pypi_darwin_py313_test.environment import (
+    ENVIRONMENT_FILE,
+    load_dotenv,
+)
 from test_pypi_darwin_py313_test.package_metadata import __version__
 
 
 __all__: Final = ["__version__"]
 """Public module attributes."""
+
+
+if __debug__:
+    beartype = importlib.import_module("beartype")
+    beartype_claw = importlib.import_module("beartype.claw")
+    beartype_claw.beartype_this_package(
+        conf=beartype.BeartypeConf(is_color=False),
+    )
+
+load_dotenv(ENVIRONMENT_FILE)
